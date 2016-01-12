68a64161336cd9a685da0035d618a5b7d789cbf9
jnf <jeremy.flores@gmail.com>
2016-01-12 08:30:35 -0800

    Add the first exercise, Hello World.

---
+++ 00-hello-world/README.md
@@ -0,0 +1,15 @@
+# Hello World
+
+Write a program that greets the user by name, or by saying "Hello, World!" if no name is given.
+
+
+## Specification
+
+The `Hello World!` program will greet me, the caller.
+
+If I tell the program my name is _Alice_, it will greet me by saying _Hello, Alice!_.
+
+If I neglect to give it my name, it will greet me by saying _Hello, World!_.
+
+## Source
+["Hello, World!"](http://en.wikipedia.org/wiki/%22Hello,_world!%22_program) is the traditional first program for beginning programming in a new language.
---
+++ 00-hello-world/hello_world_test.rb
@@ -0,0 +1,31 @@
+gem 'minitest', '>= 5.0.0'
+require 'minitest/pride'
+require 'minitest/autorun'
+require_relative 'hello_world'
+
+class HelloWorldTest < Minitest::Test
+  def test_no_name
+    assert_equal 'Hello, World!', hello_world, <<-MSG
+    When given no name, it should greet the world!
+    MSG
+  end
+
+  def test_sample_name
+    assert_equal 'Hello, Alice!', hello_world('Alice'), <<-MSG
+    When given "Alice" it should greet Alice!
+    MSG
+  end
+
+  def test_other_sample_name
+    assert_equal 'Hello, Bob!', hello_world('Bob'), <<-MSG
+    When given "Bob" it should greet Bob!
+    MSG
+  end
+
+  def test_no_strange_name
+    assert_equal 'Hello, !', hello_world(''), <<-MSG
+    When given an empty string it should have a space
+    and punctuation, though admittedly this is strange.
+    MSG
+  end
+end
