ecbc91a38c7151cefed08e712255fde274076604
Dan Roberts <dan@adadevelopersacademy.org>
2017-01-25 15:15:48 -0800

    Add skeleton code for Alouette

---
+++ 08-alouette/alouette.rb
@@ -0,0 +1,21 @@
+class Alouette
+  ALOUETTE_PARTS = [
+    "la tête",
+    "le bec",
+    "les yeux",
+    "le cou",
+    "les ailes",
+    "les pattes",
+    "la queue",
+    "le dos"
+  ]
+
+  def self.lines_for_verse(verse_number)
+  end
+
+  def self.verse(verse_number)
+  end
+
+  def self.sing
+  end
+end
