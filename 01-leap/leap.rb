acdffe518a7aba9298708fea55759387e8029537
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-02-25 22:44:28 -0800

    condensed leap_year? method to fewer lines

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,11 +1,8 @@
-
 #take a year and report if it is a leap year
 def leap_year?(year)
-  if year % 400 == 0
+  if year % 400 == 0 || year % 100 != 0 && year % 4 == 0
     return true
-  elsif year % 100 == 0 || year % 4 != 0
-    return false
   else
-    return true
+    return false
   end
 end
