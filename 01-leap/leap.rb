3bc967e616f32469184ab0c6252f4c9111178121
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-21 11:14:20 -0700

    Refactored leap year challenge

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,7 +1,3 @@
 def leap_year?(year)
-  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-    return true
-  else
-    return false
-  end
+  return true if ( year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 )
 end
