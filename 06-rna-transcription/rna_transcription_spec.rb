17a09e26ecbd4ee8c14cf0b6da1456767c62443b
Amber Lynn <griffiam@gmail.com>
2018-10-08 09:21:59 -0700

    all test pass except 1 - error

--- 06-rna-transcription/rna_transcription_spec.rb
+++ 06-rna-transcription/rna_transcription_spec.rb
@@ -12,72 +12,58 @@ describe "RNA Complement Test" do
   end
 
   it "Check rna complement of thymine is adenine" do
-    skip
     expect('A').must_equal Complement.of_dna('T')
   end
 
   it "Check rna complement of adenine is uracil" do
-    skip
     expect('U').must_equal Complement.of_dna('A')
   end
 
   it "Check rna complement" do
-    skip
     expect('UGCACCAGAAUU').must_equal Complement.of_dna('ACGTGGTCTTAA')
   end
 
   it "Check dna complement of cytosine is guanine" do
-    skip
     expect('G').must_equal Complement.of_rna('C')
   end
 
   it "Check dna complement of guanine is cytosine" do
-    skip
     expect('C').must_equal Complement.of_rna('G')
   end
 
   it "Check dna complement of uracil is adenine" do
-    skip
     expect('A').must_equal Complement.of_rna('U')
   end
 
   it "Check dna complement of adenine is thymine" do
-    skip
     expect('T').must_equal Complement.of_rna('A')
   end
 
   it "Check dna complement" do
-    skip
     expect('ACTTGGGCTGTAC').must_equal Complement.of_rna('UGAACCCGACAUG')
   end
 
   it "Check dna raises argument error" do
-    skip
     expect(proc { Complement.of_dna('U') }).must_raise ArgumentError
   end
 
   it "Check rna raises argument error" do
-    skip
     expect( proc { Complement.of_rna('T') } ).must_raise ArgumentError
   end
 
   it "Check rna raises argument error on completely invalid input" do
-    skip
     expect(proc { Complement.of_rna('XXX') }).must_raise ArgumentError
   end
 
   it "Check dna raises argument error on completely invalid input" do
-    skip
     expect( proc { Complement.of_dna('XXX') }).must_raise ArgumentError
   end
 
   it "Check dna raises argument error on partially invalid input" do
-    skip
     expect(proc { Complement.of_dna('ACGTXXXCTTAA') }).must_raise ArgumentError
   end
 
   it "Check rna raises argument error on partially invalid input" do
-    skip
     expect(proc { Complement.of_rna('UGAAXXXGACAUG') }).must_raise ArgumentError
   end
 end
